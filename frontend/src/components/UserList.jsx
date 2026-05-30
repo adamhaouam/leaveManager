1b41f2919e76ff70a05f97e3e9ac4edb8adacdff
Adam H <183345857+adamhaouam@users.noreply.github.com>
2026-05-30 18:41:00 +1000

    Add edit and delete to user list in admin panel

--- frontend/src/components/UserList.jsx
+++ frontend/src/components/UserList.jsx
@@ -1,4 +1,22 @@
-const UserList = ({ users }) => {
+import { useAuth } from "../context/AuthContext";
+import axiosInstance from "../axiosConfig";
+
+const UserList = ({ users, setUsers, setEditingUser }) => {
+  const { user } = useAuth();
+
+  const handleDelete = async (userId) => {
+    try {
+      await axiosInstance.delete(`/api/auth/users/${userId}`, {
+        headers: { Authorization: `Bearer ${user.token}` },
+      });
+      setUsers(users.filter((user) => user._id !== userId));
+    } catch (error) {
+      alert("Failed to delete user.");
+    }
+  };
+
+
+
   return (
     <div>
       {users.map((user) => (
@@ -6,6 +24,22 @@ const UserList = ({ users }) => {
           <h2 className="font-bold">{user.name}</h2>
           <p>{user.email}</p>
           <p>{user.role}</p>
+
+          <div className="mt-2 flex">
+            <button
+              onClick={() => setEditingUser(user)}
+              className="mr-2 bg-blue-400 text-white px-4 py-2 rounded"
+            >
+              Edit
+            </button>
+            <button
+              onClick={() => handleDelete(user._id)}
+              className="bg-red-500 text-white px-4 py-2 rounded"
+            >
+              Delete
+            </button>
+
+          </div>
         </div>
       ))}
     </div>
--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -8,6 +8,7 @@ const Admin = () => {
   const { user } = useAuth();
   const navigate = useNavigate();
   const [users, setUsers] = useState([]);
+  const [editingUser, setEditingUser] = useState(null);
 
   useEffect(() => {
     if (!user || user.role !== "admin") {
@@ -33,7 +34,7 @@ const Admin = () => {
   return (
     <div className="container mx-auto p-6">
       <h1 className="text-3xl font-bold mb-6">All Users</h1>
-      <UserList users={users} />
+      <UserList users={users} setUsers={setUsers} setEditingUser={setEditingUser} />
     </div>
   );
 };
